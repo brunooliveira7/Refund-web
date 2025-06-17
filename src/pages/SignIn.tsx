091fc0ae97be5142404084ef49ef090b96c308be
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-06-17 15:23:28 -0300

    feat: Simplify SignIn form using FormData
    This commit simplifies the SignIn form by utilizing FormData to handle form data submission.
    
    - Removes the individual state variables for email and password.
    - Updates the onSubmit function to an onAction function that receives FormData.
    - Accesses email and password directly from the FormData object.
    - Adds name attributes to the Input components for email and password.

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -3,39 +3,39 @@ import { Button } from "../components/Button";
 import { useState } from "react";
 
 export function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    console.log({ email, password });
+  function onAction(formData: FormData) {
+    console.log(formData.get("email"));
   }
 
   return (
-    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+    <form action={onAction} className="w-full flex flex-col gap-4">
       <Input
         required
+        name="email"
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
-        onChange={(e) => setEmail(e.target.value)}
       />
 
       <Input
         required
+        name="password"
         legend="Senha"
         type="password"
         placeholder="123456"
-        onChange={(e) => setPassword(e.target.value)}
       />
 
       <Button type="submit" isLoading={isLoading}>
         Entrar
       </Button>
 
-      <a href="/signUp" className="text-sm font-semibold text-gray-100 mt-10
-      mb-4 text-center hover:text-green-800 transition ease-linear">
+      <a
+        href="/signUp"
+        className="text-sm font-semibold text-gray-100 mt-10
+      mb-4 text-center hover:text-green-800 transition ease-linear"
+      >
         Criar conta
       </a>
     </form>
