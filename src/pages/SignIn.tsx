fef3a4f1bf6d3f3a3403a3d0a6f8794aca3f98aa
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-06-18 10:18:48 -0300

    feat: Integrate API call in SignIn form and handle success/error scenarios
    This commit integrates the API call to authenticate the user in the SignIn form and handles both success and error scenarios.
    
    - Implements the API call using the `api` service to authenticate the user.
    - Handles Zod validation errors and Axios errors, displaying appropriate error messages to the user.

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -2,6 +2,8 @@ import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 import { useActionState } from "react";
 import { z, ZodError } from "zod";
+import { api } from "../services/api";
+import { AxiosError } from "axios";
 
 const signInSchema = z.object({
   email: z.string().email({ message: "E-mail inválido" }),
@@ -18,10 +20,18 @@ export function SignIn() {
         password: formData.get("password"),
       });
 
-      console.log(data);
+      const response = await api.post("/sessions", data);
+      console.log(response.data);
+
     } catch (error) {
+      console.log(error);
+
       if (error instanceof ZodError) {
         return { message: error.issues[0].message };
+      }
+
+      if (error instanceof AxiosError) {
+        return { message: error.response?.data.message };
       }
 
       return { message: "Erro ao fazer login" };
