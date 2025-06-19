d4cf4d90c601ee86497ad70fae6b4f94864a68f4
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-06-19 09:18:58 -0300

    feat: Persist user session using localStorage and add loading state
    This commit introduces persistence of the user session using localStorage and adds a loading state to the AuthContext.
    
    - In `AuthContext.tsx`, the user session is now persisted in localStorage using the keys `@refund:user` and `@refund:token`.
    - A loading state (`isLoading`) is added to the `AuthContext` to indicate whether the user session is being loaded from localStorage.
    - The `loadUser` function retrieves the user session from localStorage and sets the session state.
    - The `isLoading` state is set to `false` after the user session is loaded.
    - In `Routes.tsx`, the `useAuth` hook is updated to include the `isLoading` state, which is used to display a loading indicator while the user session is being loaded.

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,22 +1,49 @@
 import { createContext } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type AuthContext = {
   session: null | UserAPIResponse;
   save: (data: UserAPIResponse) => void;
+  isLoading: boolean;
 };
+
+const LOCAL_STORAGE_KEY = "@refund";
 
 export const AuthContext = createContext({} as AuthContext);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [session, setSession] = useState<null | UserAPIResponse>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   function save(data: UserAPIResponse) {
+    localStorage.setItem(
+      `${LOCAL_STORAGE_KEY}:user`,
+      JSON.stringify(data.user)
+    );
+    localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token);
+
     setSession(data);
   }
 
+  function loadUser() {
+    const user = localStorage.getItem(`${LOCAL_STORAGE_KEY}:user`);
+    const token = localStorage.getItem(`${LOCAL_STORAGE_KEY}:token`);
+
+    if (user && token) {
+      setSession({
+        token,
+        user: JSON.parse(user),
+      });
+    }
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
   return (
-    <AuthContext.Provider value={{ session, save }}>
+    <AuthContext.Provider value={{ session, save, isLoading }}>
       {children}
     </AuthContext.Provider>
   );
