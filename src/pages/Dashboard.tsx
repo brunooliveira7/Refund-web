9e8c56132b9a977f45482438a56cb80c98fe5d62
Bruno Oliveira <brunoramosoliveira7@icloud.com>
2025-06-23 15:42:52 -0300

    feat: Fetch and display refunds on the dashboard
    This commit implements the functionality to fetch and display refund requests on the dashboard.
    
    - Implements the `fetchRefunds` function to retrieve refund data from the API, including name-based filtering, pagination, and error handling.
    - Uses `useEffect` to call `fetchRefunds` on component mount, initiating the initial data load.
    - Introduces state variables for managing pagination, including `page`, `totalOfPages`, and `PER_PAGE`.
    - Handles potential errors during API requests using AxiosError and displays user-friendly error messages.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,13 @@
 import { Input } from "../components/Input";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Button } from "../components/Button";
 import searchSVG from "../assets/search.svg";
 import { RefundItem, type RefundItemProps } from "../components/RefundItem";
 import { CATEGORIES } from "../utils/categories";
 import { formatCurrency } from "../utils/formatCurrency";
 import { Pagination } from "../components/Pagination";
+import { api } from "../services/api";
+import { AxiosError } from "axios";
 
 const REFUND_EXAMPLE = {
   id: "123",
@@ -15,15 +17,29 @@ const REFUND_EXAMPLE = {
   categoryImg: CATEGORIES["transport"].icon,
 };
 
+const PER_PAGE = 5;
+
 export function Dashboard() {
   const [name, setName] = useState("");
   const [page, setPage] = useState(1);
-  const [totalOfPages, setTotalOfPages] = useState(10);
+  const [totalOfPages, setTotalOfPages] = useState(0);
   const [refunds, setRefunds] = useState<RefundItemProps[]>([REFUND_EXAMPLE]);
 
-  function fetchRefunds(e: React.FormEvent) {
-    e.preventDefault();
-    console.log(name);
+  async function fetchRefunds() {
+    try {
+      const response = await api.get(
+        `/refunds?name=${name.trim()}&page=${page}&perPage=${PER_PAGE}`
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
+      }
+
+      alert("Ocorreu um erro ao buscar as solicitações.");
+    }
   }
 
   function handlePagination(action: "next" | "previous") {
@@ -37,6 +53,10 @@ export function Dashboard() {
       return prevPage;
     });
   }
+
+  useEffect(() => {
+    fetchRefunds();
+  }, []);
 
   return (
     <div className="bg-gray-500 p-10 rounded-xl md:min-w-[768px]">
